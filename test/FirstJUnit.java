e27e733ef752890e6f26e383186d4ae423011284
Thien (Theodore) P.X. NGUYEN <theodore@enclave.vn>
2015-03-02 16:49:55 +0700

    for noting purpose: inplicit wait, explicit wait, FindsByName...

--- test/FirstJUnit.java
+++ test/FirstJUnit.java
@@ -6,8 +6,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.internal.FindsById;
+import org.openqa.selenium.internal.FindsByName;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Thien (Theodore) on 3/2/2015.
@@ -25,6 +31,32 @@ public class FirstJUnit {
         Assert.assertTrue(ch2.isButtonPresent("but1"));
     }
 
+    @Test
+    public void shouldCheckAnotherButtonOnChapter2Page(){
+        Chapter2 ch2 = loadHomePage().clickChapter2();
+        Assert.assertTrue(ch2.isButtonPresent("but1"));
+    }
+
+    @Test
+    public void testExample(){
+        selenium.get("http://book.theautomatedtester.co.uk/chapter2");
+        // implicit waiting
+//        selenium.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // explicit waiting, findElement will fire NoSuchElementException if not found, we can use findElements to get around.
+        WebElement element = new WebDriverWait(selenium, 10).until(new ExpectedCondition<WebElement>() {
+            @Override
+            public WebElement apply(WebDriver webDriver) {
+                return webDriver.findElement(By.xpath("//input[@name = 'verifybutton']"));
+            }
+        });
+        ((FindsByName)selenium).findElementByName("verifybutton");
+    }
+
+    @After
+    public void teardown(){
+        selenium.quit();
+    }
+
     private Chapter2 clickAndLoadChapter2() {
         selenium.findElement(By.linkText("Chapter2")).click();
         return PageFactory.initElements(selenium, Chapter2.class);
@@ -33,16 +65,5 @@ public class FirstJUnit {
     private HomePage loadHomePage() {
         selenium.get("http://book.theautomatedtester.co.uk");
         return new HomePage(selenium);
-    }
-
-    @Test
-    public void shouldCheckAnotherButtonOnChapter2Page(){
-        Chapter2 ch2 = loadHomePage().clickChapter2();
-        Assert.assertTrue(ch2.isButtonPresent("but1"));
-    }
-
-    @After
-    public void teardown(){
-        selenium.quit();
     }
 }
