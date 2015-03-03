ccfffc35aeb772a8a2e8c63b83163ffa3823cee4
Thien (Theodore) P.X. NGUYEN <theodore@enclave.vn>
2015-03-03 09:59:54 +0700

    run test under Chrome browser, if you don't set path to include path/to/chromedriver.exe, you will comment out the line: System.setProperty...

---
+++ test/FirstChromeDriver.java
@@ -0,0 +1,33 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by Thien (Theodore) on 3/2/2015.
+ */
+public class FirstChromeDriver {
+    private WebDriver selenium;
+    @Before
+    public void setup() {
+//        System.setProperty("webdriver.chrome.driver", "D:\\Ins\\2015\\SELENIUM\\chromedriver.exe");
+        selenium = new ChromeDriver();
+        selenium.get("http://book.theautomatedtester.co.uk/chapter4");
+    }
+
+    @Test
+    public void testExample(){
+        WebElement element = selenium.findElement(By.id("selectLoad"));
+        String value = element.getAttribute("value");
+        Assert.assertEquals("Click to load the select below", value);
+    }
+
+    @After
+    public void teardown(){
+        selenium.quit();
+    }
+}
